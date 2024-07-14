1f4fdb50fcfda181e6a08ab013a1a553884c53c2
Henrique Miranda <sergioh003@gmail.com>
2024-07-14 20:20:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text className="text-blue-500">Hello World!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#282c34',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#d1d5db',
+  },
+});
